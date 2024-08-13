99d65d97f4167f9a49aef9fc776174f9948f96d8
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2024-08-13 16:47:10 +0530

    sign up

--- app/src/main/java/com/example/cheezy/MainActivity.kt
+++ app/src/main/java/com/example/cheezy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.cheezy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val text = findViewById<TextView>(R.id.signUp)
+        text.setOnClickListener{
+            val intent = Intent (this,SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/cheezy/SignUpActivity.kt
@@ -0,0 +1,16 @@
+package com.example.cheezy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+
+class SignUpActivity : AppCompatActivity(){
+
+   override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+
+
+
+}
